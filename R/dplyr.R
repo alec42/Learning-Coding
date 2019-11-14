d6166449de2c103e95e1138426e8a961588941fe
alec42 <alecvanrassel@icloud.com>
2019-11-13 22:18:12 -0500

    DPLYR, LaTeX

---
+++ R/dplyr.R
@@ -0,0 +1,16 @@
+install.packages("nycflights13")
+library(nycflights13)
+data(flights)
+library(dplyr)
+
+flights %>%
+    group_by(month) %>%
+    summarise(mean = mean(distance), sd = sd(distance))
+View(flights)
+attach(flights)
+
+flights %>%
+    filter(is.na(dep_delay) != T) %>%
+    group_by(carrier) %>%
+    summarise(mean = mean(dep_delay), sd = sd(dep_delay))
+
