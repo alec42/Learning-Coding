ac8ce5bd12afa03fed1728c78fcee883724912c9
alec42 <alecvanrassel@icloud.com>
2019-12-06 09:51:36 -0500

    LaTex, R
    LaTex
    + Ajout document de distributions intéressantes
    
    R
    + Facet wrap ggplot2

--- R/ggplot2.R
+++ R/ggplot2.R
@@ -17,6 +17,15 @@ ggplot(mpg, aes(x = displ, y = hwy, colour = class)) +
 ggplot(mpg, aes(x = displ, y = hwy, size = cyl)) + 
     geom_point()
 
+## Facetting creates subsets of the data and outputs the same graph for each
+ggplot(mpg, aes(x = displ, y = hwy)) + 
+    geom_point() + 
+    facet_wrap(~class)
+
+ggplot(mpg, aes(x = displ, y = fl)) + 
+    geom_point() + 
+    facet_wrap(~cyl)
+
 ###
 ### Vignette qui décrit les aestétiques
 ###
